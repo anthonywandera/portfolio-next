eef5bf996c5d02e57f8c74cfadc74f62eb0851fb
anthonywandera <wanderatony92@gmail.com>
2025-04-15 17:54:29 +0300

    huh

--- ui/pages/admin-page.tsx
+++ ui/pages/admin-page.tsx
@@ -1,12 +1,26 @@
 "use client";
 import { redirect } from "next/navigation";
 import { useSelector } from "react-redux";
+import Section from "../components/section";
 
 export default function AdminPage() {
-  const isAuth = useSelector<{ isAuth: boolean }>((state) => state.isAuth);
+  const isAuth = useSelector<{ admin: { isAuth: boolean } }>(
+    (state) => state.admin.isAuth
+  );
 
   // Redirect to a login page if user is not authenticated
   if (!isAuth) redirect("/admin/login");
 
-  return <h1>Admins panel</h1>;
+  return (
+    <Section id="about" center>
+      <div>
+        <h2>About Me</h2>
+        <textarea
+          name="description"
+          className="resize-none p-1 bg-[var(--theme-color-background-tint)] rounded w-full"
+          defaultValue={"hello, i miss you"}
+        />
+      </div>
+    </Section>
+  );
 }
