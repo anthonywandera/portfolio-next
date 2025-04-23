dc6086cfedff07455613a983fda80801ac361082
anthonywandera <wanderatony92@gmail.com>
2025-04-23 10:07:03 +0300

    creating data types

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,16 +1,57 @@
-export interface Project {
+import { generateRandomId } from "../utils";
+
+export class Project {
   id: string;
   name: string;
   description: string;
   image: string;
-  url: string;
+  technologies: string[] = [];
+  reviews: Review[] = [];
+  url?: string;
+
+  constructor({
+    name,
+    description,
+    image,
+    url,
+  }: {
+    name: string;
+    description: string;
+    image: string;
+    url?: string;
+  }) {
+    this.name = name;
+    this.description = description;
+    this.image = image;
+    this.url = url;
+    this.id = generateRandomId(10);
+  }
 }
 
-export interface Review {
+export class Review {
   id: string;
   name: string;
   review: string;
   rating: number;
   projectId: string;
   createdAt: string;
+
+  constructor({
+    name,
+    review,
+    rating,
+    projectId,
+  }: {
+    name: string;
+    review: string;
+    rating: number;
+    projectId: string;
+  }) {
+    this.name = name;
+    this.review = review;
+    this.rating = rating;
+    this.projectId = projectId;
+    this.id = generateRandomId(10);
+    this.createdAt = new Date().toISOString();
+  }
 }
